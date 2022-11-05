8f46e57b6c1422d5962834d37a831a3aed89ff29
RathiSelva <117533398+RathiSelva@users.noreply.github.com>
2022-11-05 20:00:26 +0000

    Add files via upload
    day 1

---
+++ day1/Phone.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class Phone {
+
+	public static void main(String[] args) {
+		
+         String mobileBrandName ="i phone";
+		 char mobileLogo = 'I';
+		 short noOfMobilePiece = 140;
+		 int modelNumber = 71000;
+		 long mobileImeiNumber = 2403400L;
+		 float mobilePrice = 82350.99F;
+		 boolean isDamaged = false;
+
+
+	System.out.println(mobileBrandName);
+	System.out.println(mobileLogo);
+	System.out.println(noOfMobilePiece);
+	System.out.println(modelNumber);
+	System.out.println(mobileImeiNumber);
+	System.out.println(mobilePrice);
+	System.out.println(isDamaged);
+}
+}
---
+++ day1/TwoWheeler.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class TwoWheeler {
+
+	public static void main(String[] args) {
+	
+	
+	int noOfWheels = 2;
+	short noOfMirrors = 30;
+	long chassisNumber = 50000L;
+	boolean isPunchtured = false;
+	String bikeName  = "Rocky";
+	double runningKM = 510.86;		
+	
+	
+
+	System.out.println(noOfWheels);
+	System.out.println(noOfMirrors);
+	System.out.println(chassisNumber);
+	System.out.println(isPunchtured);
+	System.out.println(bikeName);
+	System.out.println(runningKM);
+
+}
+}
